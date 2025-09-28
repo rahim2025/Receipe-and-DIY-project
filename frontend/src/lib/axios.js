106b55caea27b26625bc428256055f5ffa8fb31d
rahim2025 <mohammad.rahim.bhuiyan20@gmail.com>
2025-09-28 11:44:51 +0600

    env config fixed

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useLoadingStore } from '../store/useLoadingStore';
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:5001/api",
+    baseURL: import.meta.env.VITE_API_URL ,
     withCredentials: true,
 });
 
